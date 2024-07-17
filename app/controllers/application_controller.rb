2bce0af4f9f64581116f27ef266871a41f11aa77
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-17 13:17:33 +0900

    signupLoginLogout6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
 
   # CSRFトークンの検証をスキップ
   skip_before_action :verify_authenticity_token
+
+  before_action :set_cors_headers
+
+  private
+
+  def set_cors_headers
+    response.set_header('Access-Control-Allow-Origin', 'https://virtual-pet-front.vercel.app')
+    response.set_header('Access-Control-Allow-Credentials', 'true')
+    response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD')
+    response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Token')
+  end
 end
