fccf7b550823c4238a09c4f0f542af863c348350
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-21 01:28:21 +0900

    signupLoginLogout14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # CSRFトークンの検証をスキップ
-  skip_before_action :verify_authenticity_token
+  # CSRFトークンをヘッダーに含める
+  after_action :set_csrf_cookie
+
+  def csrf_token
+    render json: { csrfToken: form_authenticity_token }
+  end
+
+  private
+
+  def set_csrf_cookie
+    cookies['CSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-CSRF-Token']
+  end
 
 end
