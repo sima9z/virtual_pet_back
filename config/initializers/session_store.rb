3bcba253587ae8c4d78b0f5356af9ae01421b5e4
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-21 03:33:12 +0900

    signupLoginLogout17

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_my_app_session', same_site: :lax, secure: Rails.env.production?, domain: :all
+Rails.application.config.session_store :cookie_store, key: '_my_app_session', same_site: :none, secure: Rails.env.production?, domain: :all
