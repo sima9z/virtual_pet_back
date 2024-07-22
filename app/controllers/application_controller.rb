95fbf2fceebbe93e4c360130dad14aaae633944e
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-22 23:09:38 +0900

    signupLoginLogout20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # CSRFトークンを生成して返すエンドポイント
   def csrf_token
-    Rails.logger.debug "Session ID (csrf_token): #{session.id}"
-    Rails.logger.debug "CSRF Token (csrf_token): #{form_authenticity_token}"
     render json: { csrfToken: form_authenticity_token }
   end
 
   protected
 
   def verified_request?
-    super || valid_csrf_token?
+    valid_request = super || valid_csrf_token?
+    Rails.logger.debug "Verified request: #{valid_request}"
+    valid_request
   end
 
   def valid_csrf_token?
     token = request.headers['X-CSRF-Token']
     session_token = session[:_csrf_token]
+    form_token = form_authenticity_token
     Rails.logger.debug "CSRF Token from request: #{token}"
-    Rails.logger.debug "Session ID (valid_csrf_token): #{session.id}"
     Rails.logger.debug "CSRF Token from session: #{session_token}"
-    ActiveSupport::SecurityUtils.secure_compare(token.to_s, form_authenticity_token.to_s) ||
-      ActiveSupport::SecurityUtils.secure_compare(token.to_s, session_token.to_s)
+    Rails.logger.debug "Form authenticity token: #{form_token}"
+    token_present = token.present?
+    Rails.logger.debug "Token present: #{token_present}"
+    compare_form_token = ActiveSupport::SecurityUtils.secure_compare(token.to_s, form_token.to_s)
+    Rails.logger.debug "Token matches form token: #{compare_form_token}"
+    compare_session_token = ActiveSupport::SecurityUtils.secure_compare(token.to_s, session_token.to_s)
+    Rails.logger.debug "Token matches session token: #{compare_session_token}"
+    
+    token_present && (compare_form_token || compare_session_token)
   end
 end
