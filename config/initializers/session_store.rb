b16b5c68ff90e1b807f674773c3e3e681c6b3f2c
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-27 00:18:47 +0900

    not_skip_verify_authenticity_token11

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.session_store :redis_store, servers: [
-  {
-    url: ENV['REDIS_URL'],
-    namespace: "session"
-  },
-], expire_after: 90.minutes, key: "_#{Rails.application.class.module_parent_name.downcase}_session"
