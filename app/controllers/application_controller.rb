6808cc9cb91e0bcb3dc60db326b5559b728f62b9
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-27 00:48:30 +0900

    not_skip_verify_authenticity_token13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::API
   after_action :set_csrf_cookie
 
   def csrf_token
-    csrf_token_value = form_authenticity_token
-    Rails.logger.debug "Generated CSRF Token: #{csrf_token_value}"
-    render json: { csrfToken: csrf_token_value }
+    set_csrf_cookie
+    Rails.logger.debug "Generated CSRF Token: #{@csrf_token}"
+    render json: { csrfToken: @csrf_token }
   end
 
   def not_authenticated
@@ -22,14 +22,14 @@ class ApplicationController < ActionController::API
   private
 
   def set_csrf_cookie
-    csrf_token_value = form_authenticity_token
+    @csrf_token ||= form_authenticity_token
     cookies['CSRF-TOKEN'] = {
-      value: csrf_token_value,
+      value: @csrf_token,
       secure: Rails.env.production?,
       same_site: :none
     } if protect_against_forgery?
-    response.headers['X-CSRF-Token'] = csrf_token_value
-    Rails.logger.debug "Set CSRF Token in cookie and header: #{csrf_token_value}"
+    response.headers['X-CSRF-Token'] = @csrf_token
+    Rails.logger.debug "Set CSRF Token in cookie and header: #{@csrf_token}"
   end
 
   protected
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::API
     token = request.headers['X-CSRF-Token']
     Rails.logger.debug "CSRF Token from request: #{token}"
     Rails.logger.debug "CSRF Token from session: #{session[:_csrf_token]}"
-    token.present? && (token == form_authenticity_token || token == session[:_csrf_token])
+    token.present? && (token == session[:_csrf_token])
   end
 
 end
