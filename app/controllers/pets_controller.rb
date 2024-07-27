b29bb260c12c3b8a8d1b614d2a1103eabe77c90a
sima9z <159206331+sima9z@users.noreply.github.com>
2024-07-27 19:15:16 +0900

    catCreatAction

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,7 +2,7 @@ class PetsController < ApplicationController
   skip_before_action :require_login, only: [:check_pets]
 
   def check_pets
-    pets_exist = current_user.present? && current_user.dog.present?
+    pets_exist = current_user.present? && ( current_user.dog.present? || current_user.cat.present? )
     render json: { pets_exist: pets_exist }
   end
 end
