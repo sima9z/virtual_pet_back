a8f9502123a8ffe3796479c53bb7d037ddc1bd1b
sima9z <159206331+sima9z@users.noreply.github.com>
2024-08-01 14:17:48 +0900

    mainPet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,15 @@ class PetsController < ApplicationController
     Rails.logger.debug "Pets exist: #{pets_exist}"
     render json: { pets_exist: pets_exist }
   end
+
+  def pet_info
+    pet_type = if current_user.dog.present?
+                 'dog'
+               elsif current_user.cat.present?
+                 'cat'
+               else
+                 'none'
+               end
+    render json: { petType: pet_type }
+  end
 end
